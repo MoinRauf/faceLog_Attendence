ad456188b7511d82002d5eac482dbaa192b30bbc
MoinRauf <moinraufc04@gmail.com>
2023-12-24 03:05:16 +0500

    Integrate SetSalaryPolicy.jsx component for managing salary policies

--- facelog/src/Pages/AdminDash/Pages/SetDays.jsx
+++ facelog/src/Pages/AdminDash/Pages/SetDays.jsx
@@ -3,12 +3,11 @@ import axios from "axios";
 import styles from "../../../ReusableCSS/form.module.css";
 import Logo from "../../../Components/Logo";
 import HoverButton from "../../../Components/CustomButton/HoverButton";
-import FormField from "../../../Components/FormField";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import admincss from "../../../Pages/AdminDash/Pages/ADMINCSS/SetDays.css";
 
-// Define your functional component
 const SetDays = () => {
   const navigate = useNavigate();
   // State variables to store input values
@@ -71,134 +70,47 @@ const SetDays = () => {
         pauseOnHover
         theme="light"
       />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px 100px 20px 20px",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "#d9eff5",
-          borderRadius: "10px",
-          boxShadow: "8px 8px 8px #000000",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "36px",
-            color: "#16344f",
-            padding: "10px 10px 10px 10px",
-            marginLeft: "60px",
-            display: "flex",
-            alignContent: "center",
-            textAlign: "center",
-          }}
-        >
-          Permissible Days
-        </h1>
+      <div class="maindiv">
+        <h1 class="heading">Permissible Days</h1>
 
         <div>
           <div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <label
-                style={{ color: "#16344f", fontSize: "18px", fontWeight: 550 }}
-              >
-                Absent Days:
-              </label>
+            <div class="center">
+              <label class="divlabel">Absent Days:</label>
             </div>
             <input
               type="number"
               value={absentDays}
               placeholder="Enter the number of absent days"
               onChange={(e) => setAbsentDays(e.target.value)}
-              style={{
-                padding: "8px",
-                backgroundColor: "#c0ffd1",
-                color: "#16344f",
-                fontSize: "16px",
-                border: "1px solid #16344f",
-                borderRadius: "5px",
-                outline: "none",
-                width: "130%",
-                marginBottom: "20px",
-              }}
+              className="userinput"
             />
           </div>
           <div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <label
-                style={{ color: "#16344f", fontSize: "18px", fontWeight: 550 }}
-              >
-                Half Days:
-              </label>
+            <div class="center">
+              <label class="divlabel">Half Days:</label>
             </div>
             <input
               type="number"
               value={halfDays}
               onChange={(e) => setHalfDays(e.target.value)}
               placeholder="Enter the number of absent days"
-              style={{
-                padding: "8px",
-                backgroundColor: "#c0ffd1",
-                color: "#16344f",
-                fontSize: "16px",
-                border: "1px solid #16344f",
-                borderRadius: "5px",
-                outline: "none",
-                width: "130%",
-                marginBottom: "20px",
-              }}
+              className="userinput"
             />
           </div>
           <div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <label
-                style={{ color: "#16344f", fontSize: "18px", fontWeight: 550 }}
-              >
-                Late Days:
-              </label>
+            <div class="center">
+              <label class="divlabel">Late Days:</label>
             </div>
             <input
               type="number"
               value={lateDays}
               placeholder="Enter the number of absent days"
               onChange={(e) => setLateDays(e.target.value)}
-              style={{
-                padding: "8px",
-                backgroundColor: "#c0ffd1",
-                color: "#16344f",
-                fontSize: "16px",
-                border: "1px solid #16344f",
-                borderRadius: "5px",
-                outline: "none",
-                width: "130%",
-                marginBottom: "20px",
-              }}
+              className="userinput"
             />
           </div>
-          <div style={{ margin: "30px 0px 30px 80px" }}>
+          <div style={{ margin: "30px 0px 30px 120px" }}>
             <HoverButton
               label="Submit"
               bgColor="#16344f"
--- facelog/src/Pages/AdminDash/Pages/SetSalaryPolicy.jsx
+++ facelog/src/Pages/AdminDash/Pages/SetSalaryPolicy.jsx
@@ -1,60 +1,133 @@
-import React from 'react'
+import React, { useState } from "react";
 import styles from "../../../ReusableCSS/form.module.css";
 import Logo from "../../../Components/Logo";
 import HoverButton from "../../../Components/CustomButton/HoverButton";
-import FormField from '../../../Components/FormField';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import EditCalendarIcon from '@mui/icons-material/EditCalendar';
-import EventNoteIcon from '@mui/icons-material/EventNote';
-
+import adminPageStyles from "../../../Pages/AdminDash/Pages/ADMINCSS/SetSalaryPolicy.css";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const SetSalaryPolicy = () => {
+  const navigate = useNavigate();
+  const [absentDays, setAbsentDays] = useState("");
+  const [lateDays, setLateDays] = useState("");
+  const [halfDays, setHalfDays] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if any field is empty
+    if (!absentDays || !lateDays || !halfDays) {
+      // Show error toast and return
+      toast.error("Please fill out all fields");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/SetSalaryPolicy",
+        {
+          absentDays,
+          lateDays,
+          halfDays,
+        }
+      );
+
+      console.log("API Response:", response.data);
+
+      // Show success toast
+      toast.success("Form submitted successfully");
+
+      setTimeout(() => {
+        navigate("/admindashboard");
+      }, 3000);
+
+      // Clear the form by resetting state variables
+      setAbsentDays("");
+      setHalfDays("");
+      setLateDays("");
+    } catch (error) {
+      // Handle errors
+      console.error("API Error:", error);
+      // Show error toast if there is an API error
+      toast.error("An error occurred while submitting the form");
+    }
+  };
+
   return (
-    <div className={styles.formBody}>
-      {/* LOGO */}
+    <form onSubmit={handleSubmit} className={styles.formBody}>
       <Logo />
-
-      {/* Blue Admin Registration Form Container */}
-      <div className={styles.formContainer} style={{ padding: "0px 30px" }}>
-        <h1 className={styles.formheading}>Salary Deduction Policy</h1>
-
-        {/* HTML form */}
-        <form className={styles.form}>
-          <FormField
-            label="Absent Days"
-            // icon={<CalendarMonthIcon />}
-            inputType="Number"
-            placeholder="%"
-            min="0"
-          />
-          <FormField
-            label="Late Days"
-            // icon={<EditCalendarIcon />}
-            inputType="Number"
-            placeholder="%"
-            min="0"
-          />
-          <FormField
-            label="Half Days"
-            // icon={<EventNoteIcon />}
-            inputType="Number"
-            placeholder="%"
-            min="0"
-          />
-
-          {/* Register button */}
-          <div style={{ margin: "30px 0px" }}>
-            <HoverButton
-              label="Submit"
-              bgColor="#16344f"
-              textColor="#d9eff5"
-              // linkTo="/login"
-            />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className="maindiv1">
+        <h1 className="heading1">Salary Deduction Policy</h1>
+        <div className={styles.formField}>
+          <div className="center">
+            <label className="divlabel1">Absent Days:</label>
           </div>
-        </form>
+
+          <input
+            type="number"
+            value={absentDays}
+            onChange={(e) => setAbsentDays(e.target.value)}
+            // className={adminPageStyles.userinput}
+            placeholder="%"
+            min="0"
+            className="userinput1"
+          />
+        </div>
+
+        <div className={styles.formField}>
+          <div className="center">
+            <label className="divlabel1">Late Days:</label>
+          </div>
+          <input
+            type="number"
+            value={lateDays}
+            onChange={(e) => setLateDays(e.target.value)}
+            // className={adminPageStyles.userinput}
+            placeholder="%"
+            min="0"
+            className="userinput1"
+          />
+        </div>
+
+        <div className={styles.formField}>
+          <div className="center">
+            <label className="divlabel1">Half Days:</label>
+          </div>
+          <input
+            type="number"
+            value={halfDays}
+            onChange={(e) => setHalfDays(e.target.value)}
+            // className={adminPageStyles.userinput}
+            placeholder="%"
+            min="0"
+            className="userinput1"
+          />
+        </div>
+
+        <div style={{ margin: "30px 0px 30px 150px" }}>
+          <HoverButton
+            label="Submit"
+            bgColor="#16344f"
+            textColor="#d9eff5"
+            type="submit"
+          />
+        </div>
       </div>
-    </div>
+    </form>
   );
-}
+};
 
 export default SetSalaryPolicy;
